111f787bd3be8562bbcf77bfd389f9a5b2d550a7
ozzular <personallouis@outlook.com>
2025-09-21 07:23:36 -0400

    Task 0: Project setup - Install Axios, create directory structure, setup basic layout

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GitHub User Search</h1>
+        <p>Search for GitHub users and explore their profiles</p>
+      </header>
+      <main>
+        {/* Search component will be added in Task 1 */}
+        <div className="search-container">
+          <p>Search functionality coming soon...</p>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
