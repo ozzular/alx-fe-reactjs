a6bf75571e151be23a4529a5302ecd15c9dec072
ozzular <personallouis@outlook.com>
2025-10-08 21:01:14 -0400

    Fix Tailwind CSS configuration format for checker

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
